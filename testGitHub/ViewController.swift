a06871f9f78f45e8513dc6084b808d722a338b3b
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2017-10-22 14:06:04 +0200

    added lines

--- testGitHub/ViewController.swift
+++ testGitHub/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("1# change")
+        print("1# change")
+        print("1# change")
+
+        
     }
 
     override func didReceiveMemoryWarning() {
